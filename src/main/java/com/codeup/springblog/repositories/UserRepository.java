d5d754f7f8756adb28812dfdd8e670fe90c5f9b2
Orenthal Hill <ojhill98@gmail.com>
2021-11-21 14:48:00 -0600

    added stuff missing

--- src/main/java/com/codeup/springblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/springblog/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.codeup.springblog.repositories;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.persistence.Id;
 
 public interface UserRepository extends JpaRepository<Post, Id> {
+
+    User findByUsername(String username);
 
 }
